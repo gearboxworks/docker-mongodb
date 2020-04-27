052287a72c796b41c95b1ff7aa7799a988ac4d94
MickMake <github@mickmake.com>
2020-04-27 21:03:42 +1000

    Build with docker-template v1.4.4

---
+++ versions/4.0.18/build/unit-tests/mongodb/02-release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Created on 2020-04-27T21:02:49+1000, using template:02-release.sh.tmpl and json:gearbox.json
+
+p_info "mongodb-4.0.18" "Release test started."
+
+ps -eaf
+
+p_info "mongodb-4.0.18" "Release test finished."
