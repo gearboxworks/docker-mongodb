494e73a10a66cb8b8ad023a9ee11f4c81c5292fa
MickMake <github@mickmake.com>
2020-05-06 21:25:05 +1000

    Rebuild with docker-template v1.6.0

---
+++ versions/3.2.21/build/unit-tests/mongodb/02-release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Created on 2020-05-06T21:13:02+1000, using template:02-release.sh.tmpl and json:gearbox.json
+
+p_info "mongodb-3.2.21" "Release test started."
+
+ps -eaf
+
+p_info "mongodb-3.2.21" "Release test finished."
---
+++ versions/3.6.18/build/unit-tests/mongodb/02-release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Created on 2020-05-06T21:13:03+1000, using template:02-release.sh.tmpl and json:gearbox.json
+
+p_info "mongodb-3.6.18" "Release test started."
+
+ps -eaf
+
+p_info "mongodb-3.6.18" "Release test finished."
